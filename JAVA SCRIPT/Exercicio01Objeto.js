9128acbe6c3953c4b2ff375d1f2c253137c69ab4
Thiago Claudino <thiagolclaudino@gmail.com>
2023-10-19 22:06:45 -0300

    19/10/*2023 2.0

--- JAVA SCRIPT/Exercicio01Objeto.js
+++ JAVA SCRIPT/Exercicio01Objeto.js
@@ -1,63 +1,39 @@
 let pessoas = [];
 
-let pessoa = {
-              nome:'Marcelo'
-              idade: 44
-             }
+pessoas.push({ altura: 1.80, sexo: 'M'});
+pessoas.push({ altura: 1.90, sexo: 'M'});
+pessoas.push({ altura: 1.50, sexo: 'F'});
+pessoas.push({ altura: 1.99, sexo: 'F'});
+pessoas.push({ altura: 2.80, sexo: 'M'});
+pessoas.push({ altura: 1.30, sexo: 'M'});
 
-pessoas.push(pessoa)
-
- 
-
-alturas.push(1.80); //0
-alturas.push(1.60); //1
-alturas.push(1.50); //2
-alturas.push(1.90); //3
-alturas.push(2.90); //3
-
- 
-
-sexos.push('M'); //0
-sexos.push('M'); //1
-sexos.push('F'); //2
-sexos.push('F'); //3
-sexos.push('M'); //1
-
- 
-
-let maior = alturas[0]; //1.80
-let menor = alturas[0]; //1.80
+let maior = pessoas[0].altura; 
+let menor = pessoas[0].altura; 
 let nrM = 0;
 let nrF = 0;
 let alturaF = 0;
-for(let i=0; i < alturas.length; i++ ){
+for(let i=0; i < pessoas.length; i++ ){
     //1 questao
-    if (maior < alturas[i]) 
-       maior = alturas[i];
+    if (maior < pessoas[i].altura) 
+       maior = pessoas[i].altura;
 
- 
-
-    if (menor > alturas[i])   
-       menor = alturas[i];
-
- 
+    if (menor > pessoas[i].altura)   
+       menor = pessoas[i].altura;
 
     //2 questao
-    if (sexos[i] == 'F'){
+    if (pessoas[i].sexo == 'F'){
         nrF++;
-        alturaF += alturas[i];
+        alturaF += pessoas[i].altura;
     }   
 
- 
-
     //3 questao
-    if (sexos[i] == 'M')
+    if (pessoas[i].sexo == 'M')
       nrM++;
 }
-
- 
 
 console.log("Maior Altura: "+maior);
 console.log("Menor Altura: "+menor);
 console.log("Media Mulheres: "+(alturaF/nrF));
 console.log("Numero de Homens: "+nrM);
+
+
