3dbe2413deae123bbb4de9d47f81a52ffe44621d
Thiago Claudino <thiagolclaudino@gmail.com>
2023-10-23 21:13:42 -0300

    23/10/2023

---
+++ AVALIAÇÃO JAVA/Biblioteca.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Biblioteca {
+    public String nome;
+    public ArrayList<Livro> livros;
+
+    public static ArrayList<Biblioteca> bibliotecas = new ArrayList<>();
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+        this.livros = new ArrayList<>();
+
+        bibliotecas.add(this);
+    }
+
+    public String toString() {
+        return "Nome: " + this.nome;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        this.livros.add(livro);
+    }
+
+    public void listarLivros() {
+        for(int i = 0; i < this.livros.size(); i++) {
+            System.out.println(i + " - " + this.livros.get(i).toString());
+        }
+    }
+
+    public static void listarBibliotecas() {
+        for(int i = 0; i < bibliotecas.size(); i++) {
+            Biblioteca biblioteca = bibliotecas.get(i);
+            System.out.println(i + " - " + biblioteca.toString());
+            biblioteca.listarLivros();
+        }
+    }
+}
